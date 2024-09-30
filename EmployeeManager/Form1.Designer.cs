93c1f415edc707c7c6fcee6b4724a82050312687
JoeDevth <643070310021@npu.ac.th>
2024-09-30 23:58:13 +0700

    Add project files

---
+++ EmployeeManager/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace EmployeeManager
+{
+    partial class LoadData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.dataGridViewEmployees = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("CordiaUPC", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(30, 363);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "รหัสพนักงาน";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("CordiaUPC", 27.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(44, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 46);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ชื่อพนักงาน";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("CordiaUPC", 27.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(64, 468);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "เงินเดือน";
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtEmployeeID.Location = new System.Drawing.Point(199, 380);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(532, 20);
+            this.txtEmployeeID.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(199, 431);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(532, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(199, 485);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(532, 20);
+            this.txtSalary.TabIndex = 5;
+            // 
+            // dataGridViewEmployees
+            // 
+            this.dataGridViewEmployees.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewEmployees.BackgroundColor = System.Drawing.Color.Cornsilk;
+            this.dataGridViewEmployees.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridViewEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmployees.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewEmployees.Name = "dataGridViewEmployees";
+            this.dataGridViewEmployees.Size = new System.Drawing.Size(719, 333);
+            this.dataGridViewEmployees.TabIndex = 6;
+            this.dataGridViewEmployees.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewEmployees_CellContentClick);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("CordiaUPC", 27.75F, System.Drawing.FontStyle.Bold);
+            this.btnAdd.ForeColor = System.Drawing.Color.Green;
+            this.btnAdd.Location = new System.Drawing.Point(12, 543);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(125, 55);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "เพิ่ม";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("CordiaUPC", 27.75F, System.Drawing.FontStyle.Bold);
+            this.btnEdit.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.btnEdit.Location = new System.Drawing.Point(199, 543);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(132, 55);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "แก้ไข";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("CordiaUPC", 27.75F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.ForeColor = System.Drawing.Color.Crimson;
+            this.btnDelete.Location = new System.Drawing.Point(384, 543);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(142, 55);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "ลบ";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Font = new System.Drawing.Font("CordiaUPC", 27.75F, System.Drawing.FontStyle.Bold);
+            this.btnShowAll.Location = new System.Drawing.Point(589, 543);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(142, 55);
+            this.btnShowAll.TabIndex = 10;
+            this.btnShowAll.Text = "ดูทั้งหมด";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // LoadData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(744, 610);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridViewEmployees);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LoadData";
+            this.Text = "WindowsFormsDataEmployees";
+            this.Load += new System.EventHandler(this.LoadData_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.DataGridView dataGridViewEmployees;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnShowAll;
+    }
+}
+
---
+++ EmployeeManager/Form1.cs
@@ -0,0 +1,174 @@
+﻿using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
+using Mysqlx.Datatypes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace EmployeeManager
+{
+    public partial class LoadData : Form
+    {
+        // Connection string ที่เชื่อมต่อกับ MySQL บน XAMPP
+        string connectionString = "Server=localhost;Database=employeedb;Uid=root;Pwd=;Port=3306;";
+
+        public LoadData()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData_Load(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM employees", conn);
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dataGridViewEmployees.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void dataGridViewEmployees_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //เมื่อเลือกข้อมูลใน Grid จะให้ไปแสดงข้อมูลใน Txt, cbb
+            if (e.RowIndex == -1) //ไม่เลือกอย่างอื่นที่ไม่ใช่ข้อมูล
+                return; //จบการทำงาน
+            else
+            {
+                dataGridViewEmployees.Rows[e.RowIndex].Selected = true;
+                //ดึงข้อมูลจาก grid
+                DataGridViewRow dgr = dataGridViewEmployees.Rows[e.RowIndex];
+                txtEmployeeID.Text = dgr.Cells[0].Value.ToString();
+                txtName.Text = dgr.Cells[1].Value.ToString();
+                txtSalary.Text = dgr.Cells[2].Value.ToString();
+            }
+
+        }
+
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO Employees (empID, empName, empSalary) VALUES (@id, @name, @salary)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtEmployeeID.Text)); // ใช้ empID จากการกรอกของผู้ใช้
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@salary", Convert.ToDecimal(txtSalary.Text));
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("เพิ่มพนักงานเรียบร้อยแล้ว");
+                    ClearFields();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("เกิดข้อผิดพลาด: " + ex.Message);
+                }
+            }
+        }
+
+
+
+
+        private void ClearFields()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnEdit_Click (object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "UPDATE Employees SET empName = @name, empSalary = @salary WHERE empID = @id";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtEmployeeID.Text)); // กรอก empID ที่ต้องการแก้ไข
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@salary", Convert.ToDecimal(txtSalary.Text));
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("แก้ไขข้อมูลพนักงานเรียบร้อยแล้ว");
+                    ClearFields();
+                    ShowAllEmployees();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("เกิดข้อผิดพลาด: " + ex.Message);
+                }
+            }
+            
+        }
+
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        string query = "DELETE FROM Employees WHERE empID = @id" ;
+                        MySqlCommand cmd = new MySqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@id", Convert.ToInt32(txtEmployeeID.Text));
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("ลบพนักงานเรียบร้อยแล้ว");
+                        ClearFields();
+                        ShowAllEmployees();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("เกิดข้อผิดพลาด: " +ex.Message);
+                    }
+                }
+            }
+        }
+
+        // ฟังก์ชันแสดงข้อมูลพนักงานทั้งหมดใน DataGridView
+        private void ShowAllEmployees()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT* FROM Employees";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    System.Data.DataTable table = new System.Data.DataTable();
+                    adapter.Fill(table);
+                    dataGridViewEmployees.DataSource = table;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("เกิดข้อผิดพลาด:" + ex.Message);
+
+                }
+            }
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            ShowAllEmployees();
+        }
+
+       
+    }
+}
